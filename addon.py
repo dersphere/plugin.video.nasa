e944a1adcc06b375e0e4f1e5b7fe8c8597efe9cf
sphere <sphere@dersphere.de>
2012-02-10 08:15:23 +0100

    initial commit

---
+++ addon.py
@@ -0,0 +1,120 @@
+from xbmcswift import Plugin, xbmc, xbmcplugin, xbmcgui, clean_dict
+import resources.lib.scraper as scraper
+
+__addon_name__ = 'Nasa'
+__id__ = 'plugin.video.nasa'
+
+
+class Plugin_mod(Plugin):
+
+    def add_items(self, iterable, view_mode=None, sort_method_ids=[]):
+        items = []
+        urls = []
+        for i, li_info in enumerate(iterable):
+            items.append(self._make_listitem(**li_info))
+            if self._mode in ['crawl', 'interactive', 'test']:
+                print '[%d] %s%s%s (%s)' % (i + 1, '', li_info.get('label'),
+                                            '', li_info.get('url'))
+                urls.append(li_info.get('url'))
+        if self._mode is 'xbmc':
+            if view_mode:
+                xbmc.executebuiltin('Container.SetViewMode(%s)' % view_mode)
+            xbmcplugin.addDirectoryItems(self.handle, items, len(items))
+            for id in sort_method_ids:
+                xbmcplugin.addSortMethod(self.handle, id)
+            xbmcplugin.endOfDirectory(self.handle)
+        return urls
+
+plugin = Plugin_mod(__addon_name__, __id__, __file__)
+
+
+@plugin.route('/', default=True)
+def show_root_menu():
+    __log('show_root_menu start')
+    items = []
+    items.append({'label': plugin.get_string(30100),
+                  'url': plugin.url_for('show_streams')})
+    items.append({'label': plugin.get_string(30101),
+                  'url': plugin.url_for('show_topics')})
+
+    __log('show_root_menu end')
+    return plugin.add_items(items)
+
+
+@plugin.route('/streams/')
+def show_streams():
+    __log('show_streams start')
+    NasaScraper = scraper.NasaScraper()
+    streams = NasaScraper.get_streams()
+    items = [{'label': stream['title'],
+              'url': stream['url'],
+              'is_folder': False,
+              'is_playable': True,
+             } for stream in streams]
+    __log('show_streams finished')
+    return plugin.add_items(items)
+
+
+@plugin.route('/videos/')
+def show_topics():
+    __log('show_topics started')
+    NasaScraper = scraper.NasaScraper()
+    topics = NasaScraper.get_video_topics()
+    items = [{'label': topic['name'],
+              'url': plugin.url_for('show_videos_by_topic',
+                                    topic_id=topic['id']),
+             } for topic in topics]
+    __log('show_topics finished')
+    return plugin.add_items(items)
+
+
+@plugin.route('/videos/<topic_id>/')
+def show_videos_by_topic(topic_id):
+    __log('show_videos_by_topic started with topic_id=%s' % topic_id)
+    NasaScraper = scraper.NasaScraper()
+    videos, video_count = NasaScraper.get_videos(topic_id, start=0, limit=15,
+                                                 order_method=None, order=None)
+    items = [{'label': video['title'],
+              'thumbnail': video['thumbnail'],
+              'info': {'originaltitle': video['title'],
+                       'duration': video['duration'],
+                       'description': video['description']},
+              'url': plugin.url_for('play', id=video['id']),
+              'is_folder': False,
+              'is_playable': True,
+             } for video in videos]
+    __log('show_videos_by_topic finished')
+    return plugin.add_items(items)
+
+
+@plugin.route('/video/<id>/')
+def play(id):
+    __log('play started with id=%s' % id)
+    NasaScraper = scraper.NasaScraper()
+    video = NasaScraper.get_video(id)
+    __log('play finished with video=%s' % video)
+    return plugin.set_resolved_url(video['url'])
+
+
+@plugin.route('/search_station/')
+def search():
+    __log('search start')
+    search_string = None
+    keyboard = xbmc.Keyboard('', plugin.get_string(30201))
+    keyboard.doModal()
+    if keyboard.isConfirmed() and keyboard.getText():
+        search_string = keyboard.getText()
+        __log('search gots a string: "%s"' % search_string)
+        language = __get_language()
+        stations = NasaScraper.search_stations_by_string(language,
+                                                         search_string)
+        items = __format_stations(stations)
+        __log('search end')
+        return plugin.add_items(items)
+
+
+def __log(text):
+    xbmc.log('%s addon: %s' % (__addon_name__, text))
+
+if __name__ == '__main__':
+    plugin.run()
---
+++ resources/lib/scraper.py
@@ -0,0 +1,173 @@
+import simplejson as json
+from urllib import urlencode
+from urllib2 import urlopen, Request
+import re
+
+USER_AGENT = ('Mozilla/5.0 (Windows NT 6.1) AppleWebKit/535.7 '
+              '(KHTML, like Gecko) Chrome/16.0.912.77 Safari/535.7')
+
+REFERER = 'http://www.nasa.gov/multimedia/videogallery/index.html'
+
+API_URL = 'http://cdn-api.vmixcore.com/apis/media.php'
+
+VIDEO_LANDING_URL = ('http://www.nasa.gov/multimedia/'
+                     'videogallery/vmixVideoLanding2.js')
+
+VIDEO_TOPICS = ({'id': '131', 'name': 'All Videos'},
+                {'id': '7844', 'name': 'Aeronautics'},
+                {'id': '13851', 'name': 'Beyond Earth'},
+                {'id': '13861', 'name': 'Commercial Space'},
+                {'id': '7845', 'name': 'Earth'},
+                {'id': '13841', 'name': 'NASA History and People'},
+                {'id': '7914', 'name': 'NASA In Your Life'},
+                {'id': '7774', 'name': 'Station and Shuttle'},
+                {'id': '7773', 'name': 'Solar System'},
+                {'id': '7849', 'name': 'Technology'},
+                {'id': '7850', 'name': 'Universe'})
+
+ATOKEN = 'cf15596810c05b64c422e071473549f4'
+
+
+class NasaScraper(object):
+
+    def __init__(self, force_refresh=False):
+        self.force_refresh = force_refresh
+        self.requests = {}
+        self.atoken = self.__get_atoken()
+
+    def get_video_topics(self):
+        if self.force_refresh:
+            url = VIDEO_LANDING_URL
+            r_genre_names = re.compile('var genreNames=\[(.+?)\];')
+            r_genre_ids = re.compile('var genreIds=\[(.+?)\];')
+            html = self.__get_url(url)
+            genre_names = re.search(r_genre_names, html).group(1).split(',')
+            genre_ids = re.search(r_genre_ids, html).group(1).split(',')
+            video_topics = []
+            for genre in zip(genre_ids, genre_names):
+                video_topics.append({'id': genre[0],
+                                     'name': genre[1].strip("'")})
+        else:
+            video_topics = VIDEO_TOPICS
+        return video_topics
+
+    def get_videos(self, topic_id, start=0, limit=15,
+                   order_method=None, order=None):
+        if order_method is None or order_method not in ('DESC', 'ASC'):
+            order_method = 'DESC'
+        if order is None:
+            order = 'date_published_start'
+        if start < 0:
+            start = 0
+        if limit < 0 or limit > 250:
+            limit = 15
+        params = {'action': 'getMediaList',
+                  'class_id': 1,
+                  'alltime': 1,
+                  'order_method': order_method,
+                  'order': order,
+                  'get_count': 1,
+                  'export': 'JSONP',
+                  'start': start,
+                  'limit': limit,
+                  'metadata': 1,
+                  'atoken': self.atoken}
+        if int(topic_id) < 1000:  # just a guess...
+            params['external_genre_ids'] = topic_id
+        else:
+            params['genre_ids'] = topic_id
+        url = API_URL
+        html = self.__get_url(url, get_dict=params)
+        json_data = self.__get_json(html)
+        media = json_data['media']
+        videos = []
+        for m in media:
+            videos.append({'title': m['title'],
+                           'duration': m['duration'],
+                           'thumbnail': m['thumbnail'][0]['url'],
+                           'description': m['description'],
+                           'id': m['id']})
+        total_count = json_data['total_count']
+        return videos, total_count
+
+    def get_streams(self):
+        streams = []
+        streams.append({'title': 'Nasa TV',
+                        'url': ('rtmp://cp76072.live.edgefcs.net/live/ '
+                                'swfUrl="http://www.nasa.gov/templateimages/'
+                                'redesign/flash_player/swf/4.5/player.swf" '
+                                'pageUrl="http://www.nasa.gov/multimedia/'
+                                'nasatv/media_flash.html" '
+                                'Playpath=MED-HQ-Flash@42814 live=true')})
+        return streams
+
+    def get_video(self, id):
+        params = {'action': 'getMedia',
+                  'media_id': id,
+                  'atoken': self.atoken}
+        url = API_URL
+        html = self.__get_url(url, get_dict=params)
+        json_data = self.__get_json(html)
+        media = json_data
+        token = media['formats']['format'][-1]['token']
+        signature = self.__get_nasa_signature(token)
+        timestamp = '1325444582134'
+        p = 'token=%s&expires=%s&signature=%s' % (token, timestamp, signature)
+        download_url = 'http://media.vmixcore.com/vmixcore/download?%s' % p
+        video = {'title': media['title'],
+                 'thumbnail': media['thumbnail'][0]['url'],
+                 'url': download_url}
+        return video
+
+    def __get_nasa_signature(self, token):
+        sig_url = ('http://hscripts.vmixcore.com/clients/nasa/'
+                   'generate_signature.php?token=%s' % token)
+        t = self.__get_url(sig_url)
+        r_sig = re.compile('"signature":"(.+?)"')
+        signature = re.search(r_sig, t).group(1)
+        return signature
+
+    def __get_atoken(self):
+        if self.force_refresh:
+            url = VIDEO_LANDING_URL
+            r_atoken = re.compile('var atoken = \'(.+?)\';')
+            html = self.__get_url(url)
+            atoken = re.search(r_atoken, html).group(1)
+            log('retrieved atoken: %s' % self.atoken)
+        else:
+            atoken = ATOKEN
+        return atoken
+
+    def __get_url(self, url, get_dict='', post_dict=''):
+        log('__get_url started with url=%s, get_dict=%s, post_dict=%s'
+            % (url, get_dict, post_dict))
+        hash = '%s-%s-%s' % (url, urlencode(get_dict), urlencode(post_dict))
+        if hash in self.requests.keys():
+            log('__get_url using cache for url=%s' % url)
+            response = self.requests[hash]
+        else:
+            if get_dict:
+                full_url = '%s?%s' % (url, urlencode(get_dict))
+            else:
+                full_url = url
+            req = Request(full_url)
+            req.add_header('User-Agent', USER_AGENT)
+            req.add_header('Referer', REFERER)
+            log('__get_url opening url=%s' % full_url)
+            if post_dict:
+                response = urlopen(req, urlencode(post_dict)).read()
+            else:
+                response = urlopen(req).read()
+            self.requests[hash] = response
+            log('__get_url finished with %d bytes result' % len(response))
+        return response
+
+    def __get_json(self, html):
+        log('__get_json started')
+        json_obj = json.loads(html)
+        log('__get_json finished')
+        return json_obj
+
+
+def log(text):
+    print 'Nasa scraper: %s' % text
